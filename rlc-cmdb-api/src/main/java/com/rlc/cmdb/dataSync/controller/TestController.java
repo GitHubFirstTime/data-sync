cfd21196ed39a196ecee66e918006fd256c04e3f
zyc <zyc_12@msn.cn>
2021-04-15 20:10:30 +0800

    测试controller

---
+++ rlc-cmdb-api/src/main/java/com/rlc/cmdb/dataSync/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.rlc.cmdb.dataSync.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author rlc_zyc
+ * @version 1.0
+ * @description: TODO
+ * @date 2021/4/15 20:08
+ */
+@RestController
+public class TestController {
+}
